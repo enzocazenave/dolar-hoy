886771ff83f56be0fd1b28636cda50498cdb7574
Enzo Cazenave <encaze24@hotmail.com>
2023-05-14 23:08:25 -0300

    Add changes in app

--- dolarhoy/src/App.tsx
+++ dolarhoy/src/App.tsx
@@ -1,60 +1,116 @@
-import { SafeAreaView, ScrollView, StyleSheet, Text, TouchableOpacity, View, ActivityIndicator } from 'react-native'
+import { SafeAreaView, ScrollView, StyleSheet, Text, TouchableOpacity, View, ActivityIndicator, Animated, useColorScheme } from 'react-native'
 import { DolarType } from './components/DolarType'
 import { useEffect, useState } from 'react'
 import { type Casa } from './types.d'
 
-const excludedDollars = ['Dolar', 'Argentina', 'Bitcoin']
+const excludedDollars = ['Dolar', 'Argentina', 'Bitcoin', 'Dolar Soja']
 
 export const App: React.FC = () => {
-    const [data, setData] = useState([])
-    const [loading, setLoading] = useState(true)
+    const [data, setData] = useState<Casa[]>([])
+    const [loading, setLoading] = useState<boolean>(true)
+    const [opacity] = useState<number>(new Animated.Value(0))
+    const theme = useColorScheme<string>()
     
     useEffect(() => {
         getDollarValues()
     }, [])
 
-    const getDollarValues: Casa = async() => {
-        const data = await fetch('https://www.dolarsi.com/api/api.php?type=valoresprincipales')
-        const dataJson = await data.json()
-        const dataJsonFormatted = dataJson.map((data) => data.casa)
-        setData(dataJsonFormatted)
+    useEffect(() => {
+        if (!loading) {
+            Animated.timing(opacity, {
+                toValue: 1,
+                duration: 500,
+                useNativeDriver: true
+            }).start()
+        }
+    }, [loading])
+
+    const toggleShow = (agencia: Casa.agencia): void => {
+        setData(prevState => prevState.map(dollar => {
+            if (dollar.agencia !== agencia) return dollar
+
+            return {
+                ...dollar,
+                show: !dollar.show
+            }
+        }))
+    } 
+
+    const getDollarValues = async(): void => {
+        try {
+            const data = await fetch('https://www.dolarsi.com/api/api.php?type=valoresprincipales')
+            const dataJson = await data.json()
+            const dataJsonFormatted = dataJson.map((data) => {
+                return { 
+                    ...data.casa,
+                    show: true
+                }
+            })
+            setData(dataJsonFormatted)
+        } catch (e) {
+            console.log(e)
+            setData([])
+        } finally {
+            setLoading(false)
+        }
     }
 
     if (loading) return (
-        <SafeAreaView style={ styles.loadingPage }>
-            <ActivityIndicator size={ 50 } />
+        <SafeAreaView style={ [styles.loadingPage, { backgroundColor: theme === 'dark' ? '#141E2A' : '#FFF'  }] }>
+            <ActivityIndicator 
+                size={ 'large' }
+                color='#00B982'
+             />
+        </SafeAreaView>
+    )
+
+    if (!data.length) return (
+        <SafeAreaView style={ [styles.loadingPage, { backgroundColor: theme === 'dark' ? '#141E2A' : '#FFF'  }] }>
+            <Text style={ [styles.filtersTitle, { fontSize: 18 }] }>Servicio de cotizaciones no disponible</Text>
         </SafeAreaView>
     )
 
     return (
-        <SafeAreaView style={styles.container}>
-            <View style={styles.filters}>
-                <Text style={styles.filtersTitle}>FILTROS</Text>
+        <SafeAreaView style={[styles.container, theme === 'dark' && { backgroundColor: '#141E2A' }]}>
+            <Animated.View style={{ opacity, gap: 10 }}>
+                <View style={styles.filters}>
+                    <Text style={styles.filtersTitle}>FILTROS</Text>
 
-                <ScrollView
-                    contentContainerStyle={styles.filterButtons}
-                    horizontal
-                    showsHorizontalScrollIndicator={false}
-                >
-                    { data.map((dollar) => {
+                    <ScrollView
+                        contentContainerStyle={styles.filterButtons}
+                        horizontal
+                        showsHorizontalScrollIndicator={false}
+                    >
+                        { data.map((dollar) => {
+                            if (excludedDollars.includes(dollar.nombre)) return null
+
+                            return (
+                                <TouchableOpacity 
+                                    key={ dollar.agencia } 
+                                    style={[
+                                        styles.filterButton, 
+                                        theme === 'dark' && { backgroundColor: '#1E2733' },
+                                        dollar.show && { backgroundColor: '#00B982' }
+                                    ]}
+                                    activeOpacity={ 0.8 }
+                                    onPress={ () => { toggleShow(dollar.agencia) } }
+                                >
+                                    <Text style={[styles.filterButtonText, theme === 'dark' && { color: '#FAFAFA' }, dollar.show && { color: '#FFF' }]}>{dollar.nombre}</Text>
+                                </TouchableOpacity>
+                            )
+                        })}
+                    </ScrollView>
+                </View>
+
+                <ScrollView contentContainerStyle={ styles.values }>
+                    { data.map((dollar) => { 
                         if (excludedDollars.includes(dollar.nombre)) return null
-
-                        return (
-                            <TouchableOpacity key={ dollar.agencia } style={styles.filterButton}>
-                                <Text style={styles.filterButtonText}>{dollar.nombre}</Text>
-                            </TouchableOpacity>
-                        )
+                        if (dollar.show) return <DolarType key={ dollar.agencia } name={ dollar.nombre } purchaseValue={ dollar.compra } saleValue={ dollar.venta } />
+                    
+                        return null
                     })}
                 </ScrollView>
-            </View>
-
-            <ScrollView contentContainerStyle={ styles.values }>
-                { data.map((dollar) => {
-                    if (excludedDollars.includes(dollar.nombre)) return null
-
-                    return <DolarType key={ dollar.agencia } name={ dollar.nombre } purchaseValue={ dollar.compra } saleValue={ dollar.venta } />
-                })}
-            </ScrollView>
+            </Animated.View>
         </SafeAreaView>
     )
 }
@@ -67,7 +123,7 @@ const styles = StyleSheet.create({
     },
     container: {
         backgroundColor: '#fff',
-        gap: 10
+        height: '100%'
     },
     filters: {
         paddingHorizontal: 20,
@@ -85,7 +141,7 @@ const styles = StyleSheet.create({
     filterButton: {
         paddingVertical: 7,
         paddingHorizontal: 10,
-        backgroundColor: '#fafafa',
+        backgroundColor: '#f0f0f0',
         borderRadius: 5
     },
     filterButtonText: {
--- dolarhoy/src/components/DolarType.tsx
+++ dolarhoy/src/components/DolarType.tsx
@@ -1,4 +1,4 @@
-import { Text, View, StyleSheet } from 'react-native'
+import { Text, View, StyleSheet, useColorScheme } from 'react-native'
 import { useMemo } from 'react'
 
 interface Props {
@@ -8,27 +8,26 @@ interface Props {
 }
 
 export const DolarType: React.FC = ({ name, purchaseValue, saleValue }: Props) => {
+    const theme = useColorScheme()
     const purchaseValueRefactored = parseFloat(purchaseValue.replace(',', '.')).toFixed(2)
     const saleValueRefactored =  parseFloat(saleValue.replace(',', '.')).toFixed(2)
 
-    if ((isNaN(purchaseValueRefactored) && saleValueRefactored === '0.00') || (isNaN(saleValueRefactored) && purchaseValueRefactored === '0.00')) return null
-
     return (
-        <View style={ styles.container }>
+        <View style={ [styles.container, theme === 'dark' && { backgroundColor: '#1E2733' }] }>
             <Text style={ styles.name }>{ name.toUpperCase() }</Text>
 
             <View style={ styles.values }>
                 {!isNaN(purchaseValueRefactored) && (
                     <View style={ styles.value }>
-                        <Text style={ styles.valueTitle }>COMPRA</Text>
-                        <Text style={ styles.valueNumber }>$ { purchaseValueRefactored }</Text>
+                        <Text style={ [styles.valueTitle, theme === 'dark' && { color: '#E6E6E6' }] }>COMPRA</Text>
+                        <Text style={ [styles.valueNumber, theme === 'dark' && { color: '#FFF' }] }>$ { purchaseValueRefactored }</Text>
                     </View>
                 )}
                     
                 {!isNaN(saleValueRefactored) && (
                     <View style={ styles.value }>
-                        <Text style={ styles.valueTitle }>VENTA</Text>
-                        <Text style={ styles.valueNumber }>$ { saleValueRefactored }</Text>
+                        <Text style={ [styles.valueTitle, theme === 'dark' && { color: '#E6E6E6' }] }>VENTA</Text>
+                        <Text style={ [styles.valueNumber, theme === 'dark' && { color: '#FFF' }] }>$ { saleValueRefactored }</Text>
                     </View>
                 )}
             </View>
@@ -39,9 +38,17 @@ export const DolarType: React.FC = ({ name, purchaseValue, saleValue }: Props) =
 const styles = StyleSheet.create({
     container: {
         padding: 20,
-        backgroundColor: '#fafafa',
+        backgroundColor: '#f3f3f3',
         gap: 20,
-        borderRadius: 5
+        borderRadius: 5,
+        shadowColor: '#000',
+        shadowOffset: {
+        width: 0,
+        height: 2
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 5
     },
     name: {
         color: '#00B982',
@@ -59,7 +66,7 @@ const styles = StyleSheet.create({
     valueTitle: {
         fontSize: 18,
         textAlign: 'center',
-        color: '#606060',
+        color: '#333',
         fontWeight: 700
     },
     valueNumber: {
